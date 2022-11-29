8411f47ddb6589519df6a9c25970ae429cf47eed
0xFirekeeper <0xFirekeeper@gmail.com>
2022-11-29 04:46:31 +0300

    Eth <> Wei + ERC20 Formatting functions for Utils.cs

--- Assets/Thirdweb/Scripts/Utils.cs
+++ Assets/Thirdweb/Scripts/Utils.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Numerics;
 using Newtonsoft.Json;
 
 namespace Thirdweb
@@ -10,6 +11,7 @@ namespace Thirdweb
 
         public const string AddressZero = "0x0000000000000000000000000000000000000000";
         public const string NativeTokenAddress = "0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee";
+        public const long DECIMALS_18 = 1000000000000000000;
 
         public static string[] ToJsonStringArray(params object[] args)
         {
@@ -52,6 +54,27 @@ namespace Thirdweb
         {
             var timeSpan = (DateTime.UtcNow - new DateTime(1970, 1, 1, 0, 0, 0));
             return (long)timeSpan.TotalMilliseconds;
+        }
+
+        public static BigInteger ToWei(this double eth)
+        {
+            return (BigInteger)(eth * DECIMALS_18);
+        }
+
+        public static double ToEth(this BigInteger wei)
+        {
+            return (double)wei / DECIMALS_18;
+        }
+
+        public static string FormatERC20(this BigInteger weiValue, int decimals = 18, int decimalsToDisplay = 4)
+        {
+            double eth = (double)weiValue / Math.Pow(10.0, (double)decimals);
+            string format = "#,0";
+            if (decimalsToDisplay > 0)
+                format += ".";
+            for (int i = 0; i < decimalsToDisplay; i++)
+                format += "#";
+            return eth.ToString(format);
         }
     }
 }
