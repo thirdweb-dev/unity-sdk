345e0205d9035706fbb0e5fba634e60188cce2ae
0xFirekeeper <0xFirekeeper@gmail.com>
2023-03-01 19:38:47 +0300

    Ignore NBitcoin/HdWallet - Deployer unfinished

--- Assets/Thirdweb/Core/Scripts/Deployer.cs
+++ Assets/Thirdweb/Core/Scripts/Deployer.cs
@@ -1,4 +1,8 @@
+using System.Linq;
 using System.Threading.Tasks;
+using Thirdweb.Contracts.DropERC721;
+using Thirdweb.Contracts.DropERC721.ContractDefinition;
+using UnityEngine;
 
 namespace Thirdweb
 {
@@ -14,62 +18,164 @@ namespace Thirdweb
 
         public async Task<string> DeployNFTCollection(NFTContractDeployMetadata metadata)
         {
-            return await Bridge.InvokeRoute<string>(getRoute("deployNFTCollection"), Utils.ToJsonStringArray(metadata));
+            if (Utils.IsWebGLBuild())
+            {
+                return await Bridge.InvokeRoute<string>(getRoute("deployNFTCollection"), Utils.ToJsonStringArray(metadata));
+            }
+            else
+            {
+                throw new UnityException("This functionality is not yet available on your current platform.");
+            }
         }
 
         public async Task<string> DeployNFTDrop(NFTContractDeployMetadata metadata)
         {
-            return await Bridge.InvokeRoute<string>(getRoute("deployNFTDrop"), Utils.ToJsonStringArray(metadata));
+            if (Utils.IsWebGLBuild())
+            {
+                return await Bridge.InvokeRoute<string>(getRoute("deployNFTDrop"), Utils.ToJsonStringArray(metadata));
+            }
+            else
+            {
+                throw new UnityException("This functionality is not yet available on your current platform.");
+
+                var deploymentMessage = new DropERC721Deployment();
+                var deploymentHandler = ThirdwebManager.Instance.SDK.web3.Eth.GetContractDeploymentHandler<DropERC721Deployment>();
+                var deploymentReceipt = await deploymentHandler.SendRequestAndWaitForReceiptAsync(deploymentMessage);
+                DropERC721Service dropERC721Service = new DropERC721Service(ThirdwebManager.Instance.SDK.web3, deploymentReceipt.ContractAddress);
+                var initializeReceipt = await dropERC721Service.InitializeRequestAndWaitForReceiptAsync(
+                    defaultAdmin: await ThirdwebManager.Instance.SDK.wallet.GetAddress(),
+                    name: metadata.name,
+                    symbol: metadata.symbol,
+                    contractURI: null,
+                    trustedForwarders: metadata.trusted_forwarders.ToList(),
+                    saleRecipient: metadata.fee_recipient,
+                    royaltyRecipient: metadata.primary_sale_recipient,
+                    royaltyBps: metadata.seller_fee_basis_points,
+                    platformFeeBps: metadata.platform_fee_basis_points,
+                    platformFeeRecipient: metadata.platform_fee_recipient
+                );
+                return deploymentReceipt.ContractAddress;
+            }
         }
 
         public async Task<string> DeploySignatureDrop(NFTContractDeployMetadata metadata)
         {
-            return await Bridge.InvokeRoute<string>(getRoute("deploySignatureDrop"), Utils.ToJsonStringArray(metadata));
+            if (Utils.IsWebGLBuild())
+            {
+                return await Bridge.InvokeRoute<string>(getRoute("deploySignatureDrop"), Utils.ToJsonStringArray(metadata));
+            }
+            else
+            {
+                throw new UnityException("This functionality is not yet available on your current platform.");
+            }
         }
 
         public async Task<string> DeployMultiwrap(NFTContractDeployMetadata metadata)
         {
-            return await Bridge.InvokeRoute<string>(getRoute("deployMultiwrap"), Utils.ToJsonStringArray(metadata));
+            if (Utils.IsWebGLBuild())
+            {
+                return await Bridge.InvokeRoute<string>(getRoute("deployMultiwrap"), Utils.ToJsonStringArray(metadata));
+            }
+            else
+            {
+                throw new UnityException("This functionality is not yet available on your current platform.");
+            }
         }
 
         public async Task<string> DeployEdition(NFTContractDeployMetadata metadata)
         {
-            return await Bridge.InvokeRoute<string>(getRoute("deployEdition"), Utils.ToJsonStringArray(metadata));
+            if (Utils.IsWebGLBuild())
+            {
+                return await Bridge.InvokeRoute<string>(getRoute("deployEdition"), Utils.ToJsonStringArray(metadata));
+            }
+            else
+            {
+                throw new UnityException("This functionality is not yet available on your current platform.");
+            }
         }
 
         public async Task<string> DeployEditionDrop(NFTContractDeployMetadata metadata)
         {
-            return await Bridge.InvokeRoute<string>(getRoute("deployEditionDrop"), Utils.ToJsonStringArray(metadata));
+            if (Utils.IsWebGLBuild())
+            {
+                return await Bridge.InvokeRoute<string>(getRoute("deployEditionDrop"), Utils.ToJsonStringArray(metadata));
+            }
+            else
+            {
+                throw new UnityException("This functionality is not yet available on your current platform.");
+            }
         }
 
         public async Task<string> DeployPack(NFTContractDeployMetadata metadata)
         {
-            return await Bridge.InvokeRoute<string>(getRoute("deployPack"), Utils.ToJsonStringArray(metadata));
+            if (Utils.IsWebGLBuild())
+            {
+                return await Bridge.InvokeRoute<string>(getRoute("deployPack"), Utils.ToJsonStringArray(metadata));
+            }
+            else
+            {
+                throw new UnityException("This functionality is not yet available on your current platform.");
+            }
         }
 
         public async Task<string> DeployToken(TokenContractDeployMetadata metadata)
         {
-            return await Bridge.InvokeRoute<string>(getRoute("deployToken"), Utils.ToJsonStringArray(metadata));
+            if (Utils.IsWebGLBuild())
+            {
+                return await Bridge.InvokeRoute<string>(getRoute("deployToken"), Utils.ToJsonStringArray(metadata));
+            }
+            else
+            {
+                throw new UnityException("This functionality is not yet available on your current platform.");
+            }
         }
 
         public async Task<string> DeployTokenDrop(TokenContractDeployMetadata metadata)
         {
-            return await Bridge.InvokeRoute<string>(getRoute("deployTokenDrop"), Utils.ToJsonStringArray(metadata));
+            if (Utils.IsWebGLBuild())
+            {
+                return await Bridge.InvokeRoute<string>(getRoute("deployTokenDrop"), Utils.ToJsonStringArray(metadata));
+            }
+            else
+            {
+                throw new UnityException("This functionality is not yet available on your current platform.");
+            }
         }
 
         public async Task<string> DeployMarketplace(MarketplaceContractDeployMetadata metadata)
         {
-            return await Bridge.InvokeRoute<string>(getRoute("deployMarketplace"), Utils.ToJsonStringArray(metadata));
+            if (Utils.IsWebGLBuild())
+            {
+                return await Bridge.InvokeRoute<string>(getRoute("deployMarketplace"), Utils.ToJsonStringArray(metadata));
+            }
+            else
+            {
+                throw new UnityException("This functionality is not yet available on your current platform.");
+            }
         }
 
         public async Task<string> DeployReleasedContract(string releaserAddress, string contractName, object[] constructorParams)
         {
-            return await Bridge.InvokeRoute<string>(getRoute("deployReleasedContract"), Utils.ToJsonStringArray(releaserAddress, contractName, constructorParams));
+            if (Utils.IsWebGLBuild())
+            {
+                return await Bridge.InvokeRoute<string>(getRoute("deployReleasedContract"), Utils.ToJsonStringArray(releaserAddress, contractName, constructorParams));
+            }
+            else
+            {
+                throw new UnityException("This functionality is not yet available on your current platform.");
+            }
         }
 
         public async Task<string> DeployFromContractUri(string uri, object[] constructorParams)
         {
-            return await Bridge.InvokeRoute<string>(getRoute("deployContractFromUri"), Utils.ToJsonStringArray(uri, constructorParams));
+            if (Utils.IsWebGLBuild())
+            {
+                return await Bridge.InvokeRoute<string>(getRoute("deployContractFromUri"), Utils.ToJsonStringArray(uri, constructorParams));
+            }
+            else
+            {
+                throw new UnityException("This functionality is not yet available on your current platform.");
+            }
         }
     }
 
