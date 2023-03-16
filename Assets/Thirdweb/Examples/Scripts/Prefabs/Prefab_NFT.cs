3d8d472506fc6740248d48fea4487676766bc1ee
0xFirekeeper <0xFirekeeper@gmail.com>
2023-03-16 20:09:16 +0300

    Add button to NFT and use Test TokenERC721/1155 for loader ex

--- Assets/Thirdweb/Examples/Scripts/Prefabs/Prefab_NFT.cs
+++ Assets/Thirdweb/Examples/Scripts/Prefabs/Prefab_NFT.cs
@@ -10,10 +10,18 @@ public class Prefab_NFT : MonoBehaviour
     [Header("UI ELEMENTS")]
     public Image nftImage;
     public TMP_Text nftName;
+    public Button nftButton;
 
     public async void LoadNFT(NFT nft)
     {
         nftName.text = nft.metadata.name;
         nftImage.sprite = await ThirdwebManager.Instance.SDK.storage.DownloadImage(nft.metadata.image);
+        nftButton.onClick.RemoveAllListeners();
+        nftButton.onClick.AddListener(() => DoSomething(nft));
+    }
+
+    void DoSomething(NFT nft)
+    {
+        Debugger.Instance.Log(nft.metadata.name, nft.ToString());
     }
 }
