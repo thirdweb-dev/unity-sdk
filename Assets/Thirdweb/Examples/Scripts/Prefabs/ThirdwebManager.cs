36ded7e4136dc4a31a6bc638ae0fe70ec0deb692
0xFirekeeper <0xFirekeeper@gmail.com>
2023-04-07 21:27:05 +0300

    App Metadata can be set from the ThirdwebManager inspector

--- Assets/Thirdweb/Examples/Scripts/Prefabs/ThirdwebManager.cs
+++ Assets/Thirdweb/Examples/Scripts/Prefabs/ThirdwebManager.cs
@@ -43,13 +43,21 @@ public class ThirdwebManager : MonoBehaviour
         new ChainData("binance-testnet", "97", null),
     };
 
+    [Header("APP METADATA")]
+    public string appName = "Thirdweb Game";
+    public string appDescription = "Thirdweb Game Demo";
+    public string[] appIcons = new string[] { "https://thirdweb.com/favicon.ico" };
+    public string appUrl = "https://thirdweb.com";
+
     [Header("STORAGE OPTIONS")]
     [Tooltip("IPFS Gateway Override")]
     public string storageIpfsGatewayUrl = "https://gateway.ipfscdn.io/ipfs/";
 
     [Header("OZ DEFENDER OPTIONS")]
-    [Tooltip("Gasless Transaction Support")]
+    [Tooltip("Autotask URL")]
     public string relayerUrl = null;
+
+    [Tooltip("Forwarders can be found here https://github.com/thirdweb-dev/ozdefender-autotask")]
     public string relayerForwarderAddress = null;
 
     public ThirdwebSDK SDK;
@@ -123,6 +131,16 @@ public class ThirdwebManager : MonoBehaviour
                 openzeppelin = new ThirdwebSDK.OZDefenderOptions() { relayerUrl = this.relayerUrl, relayerForwarderAddress = this.relayerForwarderAddress, }
             };
         }
+
+        // Set up app metadata
+
+        options.wallet = new ThirdwebSDK.WalletOptions()
+        {
+            appName = string.IsNullOrEmpty(appName) ? "Thirdweb Game" : appName,
+            appDescription = string.IsNullOrEmpty(appDescription) ? "Thirdweb Game Demo" : appDescription,
+            appIcons = appIcons.Length == 0 ? new string[] { "https://thirdweb.com/favicon.ico" } : appIcons,
+            appUrl = string.IsNullOrEmpty(appUrl) ? "https://thirdweb.com" : appUrl
+        };
 
         SDK = new ThirdwebSDK(chainOrRPC, chainId, options);
     }
