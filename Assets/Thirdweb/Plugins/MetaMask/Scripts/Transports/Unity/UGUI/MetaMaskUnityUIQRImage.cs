63e9a44a2fcb26a86af3f0ceb2ed70fd445bad51
0xFirekeeper <0xFirekeeper@gmail.com>
2023-06-22 00:22:42 +0300

    warn-

--- Assets/Thirdweb/Plugins/MetaMask/Scripts/Transports/Unity/UGUI/MetaMaskUnityUIQRImage.cs
+++ Assets/Thirdweb/Plugins/MetaMask/Scripts/Transports/Unity/UGUI/MetaMaskUnityUIQRImage.cs
@@ -8,26 +8,30 @@ using ZXing.QrCode;
 
 namespace MetaMask.Transports.Unity.UI
 {
-
     public class MetaMaskUnityUIQRImage : MonoBehaviour, IMetaMaskUnityTransportListener
     {
         #region Fields
         /// <summary>The raw image to display.</summary>
         [SerializeField]
         protected RawImage rawImage;
+
         /// <summary>Should we show the QR code if the deeplink is enabled.</summary>
         [SerializeField]
         private bool showQrCodeOnDeeplink = true;
+
         /// <summary>The texture to use for deep links.</summary>
         [SerializeField]
         private Sprite[] deepLinkTexture;
+
         /// <summary>Gets or sets the index of the current sprite.</summary>
         private int _currentSpriteIndex = 0;
+
         /// <summary>Gets a value indicating whether the application is currently animating.</summary>
         /// <returns>true if the application is currently animating; otherwise, false.</returns>
         private bool _isAnimating = false;
+
         /// <summary>Gets or sets a value indicating whether the QRSwitch is on.</summary>
-        private bool _QRSwitch = false;
+        // private bool _QRSwitch = false;
         /// <summary>Gets the last URL that was copied to the script.</summary>
         /// <returns>The last URL that was copied to the script.</returns>
         private string _lastUrl = string.Empty;
@@ -35,15 +39,15 @@ namespace MetaMask.Transports.Unity.UI
 
         #region Unity Methods
 
-          /// <summary>Starts the deeplink Animation if it should be on.</summary>
+        /// <summary>Starts the deeplink Animation if it should be on.</summary>
         private void OnEnable()
         {
             if (this._isAnimating)
             {
                 StartCoroutine(Animate(this.rawImage));
             }
-            
         }
+
         /// <summary>Stops all currently active Coroutines.</summary>
         private void OnDisable()
         {
@@ -51,14 +55,14 @@ namespace MetaMask.Transports.Unity.UI
         }
 
         #endregion
-        
+
         #region Private Methods
         /// <summary>Resets the image to its original state.</summary>
         private void Reset()
         {
             this.rawImage = GetComponent<RawImage>();
         }
-      
+
         /// <summary>Called when the connection is being established.</summary>
         /// <param name="sender">The sender.</param>
         /// <param name="e">The <see cref="MetaMaskUnityConnectEventArgs"/> instance containing the event data.</param>
@@ -77,11 +81,7 @@ namespace MetaMask.Transports.Unity.UI
             var writer = new BarcodeWriter
             {
                 Format = BarcodeFormat.QR_CODE,
-                Options = new QrCodeEncodingOptions
-                {
-                    Height = height,
-                    Width = width
-                }
+                Options = new QrCodeEncodingOptions { Height = height, Width = width }
             };
             return writer.Write(textForEncoding);
         }
@@ -101,8 +101,8 @@ namespace MetaMask.Transports.Unity.UI
         #endregion
 
         #region Public Methods
-        /// <summary>Shows a QR code for the specified URL.</summary>       
-        /// <param name="url">The URL to show in the QR code.</param>       
+        /// <summary>Shows a QR code for the specified URL.</summary>
+        /// <param name="url">The URL to show in the QR code.</param>
         public void ShowQR(string url)
         {
             StopAllCoroutines();
@@ -113,7 +113,8 @@ namespace MetaMask.Transports.Unity.UI
                 {
                     this._isAnimating = true;
                     StartCoroutine(Animate(this.rawImage));
-                }else
+                }
+                else
                 {
                     this._isAnimating = true;
                 }
@@ -134,32 +135,25 @@ namespace MetaMask.Transports.Unity.UI
         /// <summary>Called when a MetaMask request is received.</summary>
         /// <param name="id">The request ID.</param>
         /// <param name="request">The request.</param>
-        public void OnMetaMaskRequest(string id, MetaMaskEthereumRequest request)
-        {
-        }
+        public void OnMetaMaskRequest(string id, MetaMaskEthereumRequest request) { }
 
         /// <summary>Called when MetaMask fails to connect.</summary>
         /// <param name="error">The error that occurred.</param>
-        public void OnMetaMaskFailure(Exception error)
-        {
-        }
+        public void OnMetaMaskFailure(Exception error) { }
 
         /// <summary>Called when the MetaMask login was successful.</summary>
-        public void OnMetaMaskSuccess()
-        {
-        }
+        public void OnMetaMaskSuccess() { }
 
         /// <summary>Switches the mode of the application.</summary>
         public void SwitchMode()
         {
-            if(!Application.isMobilePlatform)
+            if (!Application.isMobilePlatform)
             {
                 return;
             }
-            
+
             if (this._lastUrl != "")
             {
-                
                 if (this.showQrCodeOnDeeplink)
                 {
                     this.showQrCodeOnDeeplink = false;
@@ -171,7 +165,6 @@ namespace MetaMask.Transports.Unity.UI
                     ShowQR(this._lastUrl);
                 }
             }
-            
         }
 
         #endregion
@@ -181,8 +174,10 @@ namespace MetaMask.Transports.Unity.UI
         /// <param name="image">The image to animate.</param>
         /// <param name="animationSpeed">The speed of the animation.</param>
         /// <returns>An enumerator that can be used to animate the image.</returns>
-        IEnumerator Animate(RawImage image,float animationSpeed = 0.1f) {
-            while (true) {
+        IEnumerator Animate(RawImage image, float animationSpeed = 0.1f)
+        {
+            while (true)
+            {
                 image.texture = this.deepLinkTexture[_currentSpriteIndex].texture;
                 _currentSpriteIndex = (_currentSpriteIndex + 1) % this.deepLinkTexture.Length;
                 yield return new WaitForSeconds(animationSpeed);
@@ -190,7 +185,5 @@ namespace MetaMask.Transports.Unity.UI
         }
 
         #endregion
-
     }
-
 }
