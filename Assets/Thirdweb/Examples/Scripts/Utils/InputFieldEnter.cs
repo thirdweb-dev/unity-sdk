301cf43afc48c01b7e4a062421d67a7e7acbc64e
0xFirekeeper <0xFirekeeper@gmail.com>
2023-06-30 21:47:05 +0300

    Larger connected state + onSubmit "submit" axis input fields

---
+++ Assets/Thirdweb/Examples/Scripts/Utils/InputFieldEnter.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(TMP_InputField))]
+public class InputFieldEnter : MonoBehaviour
+{
+    public Button SubmitButton;
+
+    private TMP_InputField _inputField;
+
+    private void Awake()
+    {
+        _inputField = GetComponent<TMP_InputField>();
+        _inputField.onSubmit.AddListener((s) => SubmitButton.onClick.Invoke());
+    }
+}
