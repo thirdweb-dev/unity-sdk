b8250c9ac6b59b514a2577382418580c401f4c4a
0xFirekeeper <0xFirekeeper@gmail.com>
2023-10-05 04:57:19 +0300

    Mobile WebView (WIP)

--- Assets/Thirdweb/Core/Scripts/WalletsUI/EmbeddedWalletUI.cs
+++ Assets/Thirdweb/Core/Scripts/WalletsUI/EmbeddedWalletUI.cs
@@ -7,10 +7,10 @@ using Thirdweb.Redcode.Awaiting;
 using Cdm.Authentication.Browser;
 using System.Web;
 using Newtonsoft.Json;
-using System.Runtime.Serialization;
 using UnityEngine.Networking;
 using Newtonsoft.Json.Linq;
 using System;
+using Thirdweb.WebView;
 
 namespace Thirdweb.Wallets
 {
@@ -25,7 +25,9 @@ namespace Thirdweb.Wallets
         private EmbeddedWallet _embeddedWallet;
         private string _email;
         private User _user;
-        private System.Exception _exception;
+        private Exception _exception;
+        private WebViewObject _webView;
+        private string _redirectUrl;
 
         private void Awake()
         {
@@ -54,20 +56,16 @@ namespace Thirdweb.Wallets
             {
                 EmbeddedWalletCanvas.SetActive(false);
 
-                string loginUrl =
-                    $"https://ews.thirdweb.com/sdk/2022-08-12/embedded-wallet/auth/headless-google-login-managed-unity?developerClientId={ThirdwebManager.Instance.SDK.session.Options.clientId}";
-                var crossPlatformBrowser = new CrossPlatformBrowser();
-                crossPlatformBrowser.platformBrowsers.Add(RuntimePlatform.LinuxEditor, new StandaloneBrowser());
-                crossPlatformBrowser.platformBrowsers.Add(RuntimePlatform.LinuxPlayer, new StandaloneBrowser());
-                crossPlatformBrowser.platformBrowsers.Add(RuntimePlatform.WindowsEditor, new StandaloneBrowser());
-                crossPlatformBrowser.platformBrowsers.Add(RuntimePlatform.WindowsPlayer, new StandaloneBrowser());
-                crossPlatformBrowser.platformBrowsers.Add(RuntimePlatform.OSXEditor, new StandaloneBrowser());
-                crossPlatformBrowser.platformBrowsers.Add(RuntimePlatform.OSXPlayer, new StandaloneBrowser());
-                crossPlatformBrowser.platformBrowsers.Add(RuntimePlatform.IPhonePlayer, new ASWebAuthenticationSessionBrowser());
-                crossPlatformBrowser.platformBrowsers.Add(RuntimePlatform.Android, new DeepLinkBrowser());
+                if (Application.isMobilePlatform)
+                {
+                    await LoginWithWebView();
+                }
+                else
+                {
+                    await LoginWithBrowser();
+                }
 
-                var res = await crossPlatformBrowser.StartAsync(loginUrl, "http://localhost:3000/");
-                string decodedUrl = HttpUtility.UrlDecode(res.redirectUrl);
+                string decodedUrl = HttpUtility.UrlDecode(_redirectUrl);
                 Uri uri = new(decodedUrl);
                 string queryString = uri.Query;
                 var queryDict = HttpUtility.ParseQueryString(queryString);
@@ -78,15 +76,14 @@ namespace Thirdweb.Wallets
             }
             else
             {
-                SubmitButton.onClick.AddListener(OnSubmitOTP);
-                await OnSendOTP();
-                EmbeddedWalletCanvas.SetActive(true);
-                await new WaitUntil(() => _user != null || _exception != null);
-                EmbeddedWalletCanvas.SetActive(false);
-                if (_exception != null)
-                    throw _exception;
-                return _user;
+                return await LoginWithOTP();
             }
+        }
+
+        public void OnRedirectDetected(string url)
+        {
+            _redirectUrl = url;
+            _webView.SetVisibility(false);
         }
 
         public void Cancel()
@@ -132,6 +129,102 @@ namespace Thirdweb.Wallets
         private void WebViewCallback(string msg)
         {
             ThirdwebDebug.Log($"WebViewCallback: {msg}");
+        }
+
+        private async Task LoginWithBrowser()
+        {
+            string loginUrl =
+                $"https://ews.thirdweb.com/sdk/2022-08-12/embedded-wallet/auth/headless-google-login-managed-unity?developerClientId={ThirdwebManager.Instance.SDK.session.Options.clientId}";
+            var standaloneBrowser = new StandaloneBrowser();
+            var res = await standaloneBrowser.StartAsync(loginUrl, "http://localhost:3000/");
+            _redirectUrl = res.redirectUrl;
+        }
+
+        private async Task<User> LoginWithOTP()
+        {
+            SubmitButton.onClick.AddListener(OnSubmitOTP);
+            await OnSendOTP();
+            EmbeddedWalletCanvas.SetActive(true);
+            await new WaitUntil(() => _user != null || _exception != null);
+            EmbeddedWalletCanvas.SetActive(false);
+            if (_exception != null)
+                throw _exception;
+            return _user;
+        }
+
+        private async Task LoginWithWebView()
+        {
+            string platform = UnityWebRequest.EscapeURL("mobile");
+            string authProvider = UnityWebRequest.EscapeURL("google");
+            string baseUrl = UnityWebRequest.EscapeURL("https://ews.thirdweb.com");
+
+            string loginUrl;
+            string getUrl = $"https://ews.thirdweb.com/api/2022-08-12/embedded-wallet/headless-login-link?platform={platform}&authProvider={authProvider}&baseUrl={baseUrl}";
+
+            using (UnityWebRequest req = UnityWebRequest.Get(getUrl))
+            {
+                await req.SendWebRequest();
+
+                if (req.result != UnityWebRequest.Result.Success)
+                    throw new UnityException("Failed to get login link!");
+
+                loginUrl = JsonConvert.DeserializeObject<JObject>(req.downloadHandler.text)["googleLoginLink"].ToString();
+            }
+
+            if (!_webView)
+            {
+                _webView = gameObject.AddComponent<WebViewObject>();
+            }
+
+            _webView.Init(
+                cb: (msg) =>
+                {
+                    // Handle messages sent from the WebView
+                    ThirdwebDebug.Log(string.Format("CallFromJS[{0}]", msg));
+                },
+                ua: "Mozilla/5.0 (Linux; Android 11; Pixel 4a) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Mobile Safari/537.36"
+            );
+
+            _webView.EvaluateJS(
+                @"
+                    window.addEventListener('userLoginSuccess', function(event) {
+                        unityObject.SendMessage('EmbeddedWalletUI', 'OnLoginSuccess', event.detail);
+                    });
+
+                    window.addEventListener('userLoginFailed', function(event) {
+                        unityObject.SendMessage('EmbeddedWalletUI', 'OnLoginFailed', event.detail);
+                    });
+
+                    window.addEventListener('message', function(event) {
+                        if (event.data && event.data.eventType === 'injectDeveloperClientId') {
+                            event.source.postMessage({
+                                eventType: 'injectDeveloperClientIdResult',
+                                developerClientId: '"
+                    + ThirdwebManager.Instance.SDK.session.Options.clientId
+                    + @"',
+                                redirectUrl: 'myapp://'
+                            }, event.origin);
+                        }
+                    });
+                "
+            );
+
+            _webView.LoadURL(loginUrl);
+            _webView.SetVisibility(true);
+        }
+
+        // WebView Callback, handle successful authentication
+        public void OnLoginSuccess(string authResult)
+        {
+            _webView.SetVisibility(false);
+            ThirdwebDebug.Log($"Authentication Success: {authResult}");
+        }
+
+        // WebView Callback, handle failed authentication
+        public void OnLoginFailed(string error)
+        {
+            _webView.SetVisibility(false);
+            ThirdwebDebug.LogError($"Authentication Failed: {error}");
         }
     }
 }
