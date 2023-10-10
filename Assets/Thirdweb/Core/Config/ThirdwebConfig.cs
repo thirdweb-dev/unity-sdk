67315c8e1aea4a886de070c3d648bf4ad59ddb47
0xFirekeeper <0xFirekeeper@gmail.com>
2023-10-10 20:54:41 +0300

    Update tooltip

--- Assets/Thirdweb/Core/Config/ThirdwebConfig.cs
+++ Assets/Thirdweb/Core/Config/ThirdwebConfig.cs
@@ -5,7 +5,7 @@ namespace Thirdweb
     [CreateAssetMenu(fileName = "ThirdwebConfig", menuName = "Thirdweb/Configuration", order = 1)]
     public class ThirdwebConfig : ScriptableObject
     {
-        [Header("When using OAuth2 (e.g. Google) to login on mobile, you can provide a redirect URL such as 'myapp://'.")]
+        [Header("When using OAuth2 (e.g. Google) to login on mobile, you can provide a redirect URL such as 'myapp://'.\nMake sure you do a Clean Build every time you update it.")]
         public string customScheme;
     }
 }
