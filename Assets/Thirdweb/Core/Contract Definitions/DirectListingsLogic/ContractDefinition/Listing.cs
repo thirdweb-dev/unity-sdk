413324a4862de30baa87e83c6a822cdd94748a6e
0xFirekeeper <0xFirekeeper@gmail.com>
2024-01-18 19:27:13 +0300

    Update Marketplace contract definitions to v5+

--- Assets/Thirdweb/Core/Contract Definitions/DirectListingsLogic/ContractDefinition/Listing.cs
+++ Assets/Thirdweb/Core/Contract Definitions/DirectListingsLogic/ContractDefinition/Listing.cs
@@ -14,37 +14,37 @@ namespace Thirdweb.Contracts.DirectListingsLogic.ContractDefinition
         [Parameter("uint256", "listingId", 1)]
         public virtual BigInteger ListingId { get; set; }
 
-        [Parameter("address", "listingCreator", 2)]
-        public virtual string ListingCreator { get; set; }
-
-        [Parameter("address", "assetContract", 3)]
-        public virtual string AssetContract { get; set; }
-
-        [Parameter("uint256", "tokenId", 4)]
+        [Parameter("uint256", "tokenId", 2)]
         public virtual BigInteger TokenId { get; set; }
 
-        [Parameter("uint256", "quantity", 5)]
+        [Parameter("uint256", "quantity", 3)]
         public virtual BigInteger Quantity { get; set; }
 
-        [Parameter("address", "currency", 6)]
-        public virtual string Currency { get; set; }
-
-        [Parameter("uint256", "pricePerToken", 7)]
+        [Parameter("uint256", "pricePerToken", 4)]
         public virtual BigInteger PricePerToken { get; set; }
 
-        [Parameter("uint128", "startTimestamp", 8)]
+        [Parameter("uint128", "startTimestamp", 5)]
         public virtual BigInteger StartTimestamp { get; set; }
 
-        [Parameter("uint128", "endTimestamp", 9)]
+        [Parameter("uint128", "endTimestamp", 6)]
         public virtual BigInteger EndTimestamp { get; set; }
 
-        [Parameter("bool", "reserved", 10)]
-        public virtual bool Reserved { get; set; }
+        [Parameter("address", "listingCreator", 7)]
+        public virtual string ListingCreator { get; set; }
 
-        [Parameter("uint8", "tokenType", 11)]
+        [Parameter("address", "assetContract", 8)]
+        public virtual string AssetContract { get; set; }
+
+        [Parameter("address", "currency", 9)]
+        public virtual string Currency { get; set; }
+
+        [Parameter("uint8", "tokenType", 10)]
         public virtual byte TokenType { get; set; }
 
-        [Parameter("uint8", "status", 12)]
+        [Parameter("uint8", "status", 11)]
         public virtual byte Status { get; set; }
+
+        [Parameter("bool", "reserved", 12)]
+        public virtual bool Reserved { get; set; }
     }
 }
