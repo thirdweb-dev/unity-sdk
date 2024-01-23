e8ece9227b644bef3065998608a1cd041edb2c6c
0xFirekeeper <0xFirekeeper@gmail.com>
2024-01-24 02:09:14 +0300

    Improve behavior, add cancel button

--- Assets/Thirdweb/Core/Scripts/WalletsUI/WalletConnectUI.cs
+++ Assets/Thirdweb/Core/Scripts/WalletsUI/WalletConnectUI.cs
@@ -7,6 +7,8 @@ using WalletConnectUnity.Core;
 using System.Numerics;
 using WalletConnectSharp.Sign.Models.Engine;
 using WalletConnectSharp.Sign.Models;
+using System;
+using Thirdweb.Redcode.Awaiting;
 
 namespace Thirdweb.Wallets
 {
@@ -18,6 +20,8 @@ namespace Thirdweb.Wallets
         public string[] SupportedMethods = new[] { "eth_sendTransaction", "personal_sign", "eth_signTypedData_v4" };
 
         public static WalletConnectUI Instance { get; private set; }
+
+        protected Exception _exception;
 
         private void Awake()
         {
@@ -35,49 +39,55 @@ namespace Thirdweb.Wallets
 
         public virtual async Task Connect(string walletConnectProjectId, BigInteger chainId)
         {
-            try
-            {
+            _exception = null;
+
+            if (!WalletConnect.Instance.IsInitialized)
                 await WalletConnect.Instance.InitializeAsync();
 
-                var sessionResumed = await WalletConnect.Instance.TryResumeSessionAsync();
-                if (!sessionResumed)
-                {
-                    WalletConnectCanvas.SetActive(true);
-
-                    var connectOptions = new ConnectOptions
-                    {
-                        RequiredNamespaces = new RequiredNamespaces
-                        {
-                            {
-                                "eip155",
-                                new ProposedNamespace
-                                {
-                                    Methods = SupportedMethods,
-                                    Chains = new[] { $"eip155:{chainId}" },
-                                    Events = new[] { "chainChanged", "accountsChanged" },
-                                }
-                            }
-                        },
-                    };
-
-                    var connectData = await WalletConnect.Instance.ConnectAsync(connectOptions);
-
-                    var qrCodeAsTexture2D = GenerateQRTexture(connectData.Uri);
-                    QRCodeImage.sprite = Sprite.Create(qrCodeAsTexture2D, new Rect(0, 0, qrCodeAsTexture2D.width, qrCodeAsTexture2D.height), new UnityEngine.Vector2(0.5f, 0.5f));
-                    QRCodeImage.mainTexture.filterMode = FilterMode.Point;
-
-                    DeepLinkButton.onClick.RemoveAllListeners();
-                    DeepLinkButton.onClick.AddListener(() => Application.OpenURL(connectData.Uri));
-
-                    await connectData.Approval;
-                }
-                WalletConnectCanvas.SetActive(false);
-            }
-            catch
+            var sessionResumed = await WalletConnect.Instance.TryResumeSessionAsync();
+            if (!sessionResumed)
             {
-                WalletConnectCanvas.SetActive(false);
-                throw;
+                WalletConnectCanvas.SetActive(true);
+
+                var connectOptions = new ConnectOptions
+                {
+                    RequiredNamespaces = new RequiredNamespaces
+                    {
+                        {
+                            "eip155",
+                            new ProposedNamespace
+                            {
+                                Methods = SupportedMethods,
+                                Chains = new[] { $"eip155:{chainId}" },
+                                Events = new[] { "chainChanged", "accountsChanged" },
+                            }
+                        }
+                    },
+                };
+
+                var connectData = await WalletConnect.Instance.ConnectAsync(connectOptions);
+
+                var qrCodeAsTexture2D = GenerateQRTexture(connectData.Uri);
+                QRCodeImage.sprite = Sprite.Create(qrCodeAsTexture2D, new Rect(0, 0, qrCodeAsTexture2D.width, qrCodeAsTexture2D.height), new UnityEngine.Vector2(0.5f, 0.5f));
+                QRCodeImage.mainTexture.filterMode = FilterMode.Point;
+
+                DeepLinkButton.onClick.RemoveAllListeners();
+                DeepLinkButton.onClick.AddListener(() => Application.OpenURL(connectData.Uri));
+
+                var task = connectData.Approval;
+                await new WaitUntil(() => task.IsCompleted || _exception != null);
+                if (_exception != null)
+                {
+                    WalletConnectCanvas.SetActive(false);
+                    throw _exception;
+                }
             }
+            WalletConnectCanvas.SetActive(false);
+        }
+
+        public virtual void Cancel()
+        {
+            _exception = new UnityException("User cancelled");
         }
 
         public virtual Texture2D GenerateQRTexture(string text)
