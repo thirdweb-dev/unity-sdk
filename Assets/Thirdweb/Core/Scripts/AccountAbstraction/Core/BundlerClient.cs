135bd620c666ba6b589d0188ff8517cb89e98d42
0xFirekeeper <0xFirekeeper@gmail.com>
2024-01-26 00:55:14 +0300

    Update BundlerClient.cs

--- Assets/Thirdweb/Core/Scripts/AccountAbstraction/Core/BundlerClient.cs
+++ Assets/Thirdweb/Core/Scripts/AccountAbstraction/Core/BundlerClient.cs
@@ -55,7 +55,7 @@ namespace Thirdweb.AccountAbstraction
             var httpRequestMessage = new HttpRequestMessage(HttpMethod.Post, url) { Content = new StringContent(requestMessageJson, System.Text.Encoding.UTF8, "application/json") };
             if (new Uri(url).Host.EndsWith(".thirdweb.com"))
             {
-                httpRequestMessage.Headers.Add("x-sdk-name", "unity");
+                httpRequestMessage.Headers.Add("x-sdk-name", "UnitySDK");
                 httpRequestMessage.Headers.Add("x-sdk-os", Utils.GetRuntimePlatform());
                 httpRequestMessage.Headers.Add("x-sdk-platform", "unity");
                 httpRequestMessage.Headers.Add("x-sdk-version", ThirdwebSDK.version);
