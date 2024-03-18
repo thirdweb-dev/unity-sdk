856efc6601ded68cdbb45fad137ea856d81179d0
0xFirekeeper <0xFirekeeper@gmail.com>
2024-03-19 00:58:55 +0300

    ver

--- Assets/Thirdweb/Core/Scripts/ThirdwebSDK.cs
+++ Assets/Thirdweb/Core/Scripts/ThirdwebSDK.cs
@@ -286,7 +286,7 @@ namespace Thirdweb
 
         public ThirdwebSession session;
 
-        internal const string version = "4.7.10";
+        internal const string version = "4.7.11";
 
         /// <summary>
         /// Create an instance of the Thirdweb SDK.
--- Assets/Thirdweb/Editor/ThirdwebManagerEditor.cs
+++ Assets/Thirdweb/Editor/ThirdwebManagerEditor.cs
@@ -50,7 +50,7 @@ namespace Thirdweb
         private GUIContent warningIcon;
         private Texture2D bannerImage;
 
-        private static readonly string ExpandedStateKey = "ThirdwebManagerEditor_ExpandedState_4.7.10";
+        private static readonly string ExpandedStateKey = "ThirdwebManagerEditor_ExpandedState_4.7.11";
 
         private void OnEnable()
         {
