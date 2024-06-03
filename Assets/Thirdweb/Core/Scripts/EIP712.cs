f178c0ec83b39bf12cce302d6d9968e6f49950ac
0xFirekeeper <0xFirekeeper@gmail.com>
2024-06-04 02:29:04 +0300

    ZkSync Native Account Abstraction Support
    Managed through thirdweb infrastructure

--- Assets/Thirdweb/Core/Scripts/EIP712.cs
+++ Assets/Thirdweb/Core/Scripts/EIP712.cs
@@ -8,12 +8,18 @@ using TokenERC721Contract = Thirdweb.Contracts.TokenERC721.ContractDefinition;
 using TokenERC1155Contract = Thirdweb.Contracts.TokenERC1155.ContractDefinition;
 using MinimalForwarder = Thirdweb.Contracts.Forwarder.ContractDefinition;
 using AccountContract = Thirdweb.Contracts.Account.ContractDefinition;
+using System;
+using System.Collections.Generic;
+using Nethereum.Model;
+using Nethereum.Hex.HexConvertors.Extensions;
+using Nethereum.RLP;
+using System.Linq;
 
 namespace Thirdweb
 {
     public static class EIP712
     {
-        /// SIGNATURE GENERATION ///
+        #region Signature Generation
 
         public async static Task<string> GenerateSignature_MinimalForwarder(
             ThirdwebSDK sdk,
@@ -147,6 +153,16 @@ namespace Thirdweb
             return await sdk.Wallet.SignTypedDataV4(accountMessage, typedData);
         }
 
+        public static async Task<string> GenerateSignature_ZkSyncTransaction(ThirdwebSDK sdk, string domainName, string version, BigInteger chainId, AccountAbstraction.ZkSyncAATransaction transaction)
+        {
+            var typedData = GetTypedDefinition_ZkSyncTransaction(domainName, version, chainId);
+            var signatureHex = await sdk.Wallet.SignTypedDataV4(transaction, typedData);
+            var signatureRaw = EthECDSASignatureFactory.ExtractECDSASignature(signatureHex);
+            return SerializeEip712(transaction, signatureRaw, chainId);
+        }
+
+        #endregion
+
         #region Typed Data Definitions
 
         public static TypedData<Domain> GetTypedDefinition_TokenERC20(string domainName, string version, BigInteger chainId, string verifyingContract)
@@ -244,10 +260,63 @@ namespace Thirdweb
                 PrimaryType = nameof(AccountMessage),
             };
         }
-    }
+
+        public static TypedData<DomainWithNameVersionAndChainId> GetTypedDefinition_ZkSyncTransaction(string domainName, string version, BigInteger chainId)
+        {
+            return new TypedData<DomainWithNameVersionAndChainId>
+            {
+                Domain = new DomainWithNameVersionAndChainId
+                {
+                    Name = domainName,
+                    Version = version,
+                    ChainId = chainId,
+                },
+                Types = MemberDescriptionFactory.GetTypesMemberDescription(typeof(DomainWithNameVersionAndChainId), typeof(AccountAbstraction.ZkSyncAATransaction)),
+                PrimaryType = "Transaction",
+            };
+        }
 
         #endregion
 
+        #region Helpers
+
+        private static string SerializeEip712(AccountAbstraction.ZkSyncAATransaction transaction, EthECDSASignature signature, BigInteger chainId)
+        {
+            if (chainId == 0)
+            {
+                throw new ArgumentException("Chain ID must be provided for EIP712 transactions!");
+            }
+
+            var fields = new List<byte[]>
+            {
+                transaction.Nonce == 0 ? new byte[0] : transaction.Nonce.ToByteArray(isUnsigned: true, isBigEndian: true),
+                transaction.MaxPriorityFeePerGas == 0 ? new byte[0] : transaction.MaxPriorityFeePerGas.ToByteArray(isUnsigned: true, isBigEndian: true),
+                transaction.MaxFeePerGas.ToByteArray(isUnsigned: true, isBigEndian: true),
+                transaction.GasLimit.ToByteArray(isUnsigned: true, isBigEndian: true),
+                transaction.To.ToByteArray(isUnsigned: true, isBigEndian: true),
+                transaction.Value == 0 ? new byte[0] : transaction.Value.ToByteArray(isUnsigned: true, isBigEndian: true),
+                transaction.Data == null ? new byte[0] : transaction.Data,
+            };
+
+            fields.Add(signature.IsVSignedForYParity() ? new byte[] { 0x1b } : new byte[] { 0x1c });
+            fields.Add(signature.R);
+            fields.Add(signature.S);
+
+            fields.Add(chainId.ToByteArray(isUnsigned: true, isBigEndian: true));
+            fields.Add(transaction.From.ToByteArray(isUnsigned: true, isBigEndian: true));
+
+            // Add meta
+            fields.Add(transaction.GasPerPubdataByteLimit.ToByteArray(isUnsigned: true, isBigEndian: true));
+            fields.Add(new byte[] { }); // TODO: FactoryDeps
+            fields.Add(signature.CreateStringSignature().HexToByteArray());
+            // add array of rlp encoded paymaster/paymasterinput
+            fields.Add(RLP.EncodeElement(transaction.Paymaster.ToByteArray(isUnsigned: true, isBigEndian: true)).Concat(RLP.EncodeElement(transaction.PaymasterInput)).ToArray());
+
+            return "0x71" + RLP.EncodeDataItemsAsElementOrListAndCombineAsList(fields.ToArray(), new int[] { 13, 15 }).ToHex();
+        }
+
+        #endregion
+    }
 
     public partial class AccountMessage : AccountMessageBase { }
 
