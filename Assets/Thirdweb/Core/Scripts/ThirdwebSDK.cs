310870312908f29015b87c4df8550744ce605063
0xFirekeeper <0xFirekeeper@gmail.com>
2024-06-21 22:14:15 +0300

    ver

--- Assets/Thirdweb/Core/Scripts/ThirdwebSDK.cs
+++ Assets/Thirdweb/Core/Scripts/ThirdwebSDK.cs
@@ -251,7 +251,7 @@ namespace Thirdweb
 
         public ThirdwebSession Session { get; internal set; }
 
-        internal const string version = "4.16.4";
+        internal const string version = "4.16.5";
 
         /// <summary>
         /// Create an instance of the Thirdweb SDK.
